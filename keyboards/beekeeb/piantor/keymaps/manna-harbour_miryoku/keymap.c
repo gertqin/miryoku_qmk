3bc8ad03368de79b30eb3dc1b9636a5af457030a
Gert Qin Hansen <gertqin@gmail.com>
2023-11-04 16:28:12 +0100

    My miryoku keeb

--- keyboards/beekeeb/piantor/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/manna-harbour_miryoku/keymap.c
@@ -3,4 +3,27 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#pragma once
+#include QMK_KEYBOARD_H
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LGUI_T(KC_A):
+        case LGUI_T(KC_O):
+            return TAPPING_TERM + 50;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LALT_T(KC_R):
+        case LALT_T(KC_I):
+        case LSFT_T(KC_T):
+        case LSFT_T(KC_N):
+            return true;
+        default:
+            // Do not select the hold action when another key is tapped.
+            return false;
+    }
+}
